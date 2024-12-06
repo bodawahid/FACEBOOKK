0cafd5ccdde9a31d40701ab3bfae14cc21c0285a
abdo5200 <abdelrahman.mamdouh2200@gmail.com>
2024-12-06 21:49:14 +0200

    no idea

--- resources/js/component/ProfileInformation.vue
+++ resources/js/component/ProfileInformation.vue
@@ -1,28 +1,58 @@
 <template>
     <div class="col-lg-12">
         <div class="card w-100 border-0 p-0 bg-white shadow-xss rounded-xxl">
-            <div class="card-body h250 p-0 rounded-xxl overflow-hidden m-3"><img src="images/u-bg.jpg" alt="image">
+            <div class="card-body h250 p-0 rounded-xxl overflow-hidden m-3">
+                <img src="images/u-bg.jpg" alt="image" />
             </div>
             <div class="card-body p-0 position-relative">
-                <figure class="avatar position-absolute w100 z-index-1" style="top:-40px; left: 30px;"><img
-                        src="images/user-12.png" alt="image" class="float-right p-1 bg-white rounded-circle w-100">
+                <figure
+                    class="avatar position-absolute w100 z-index-1"
+                    style="top: -40px; left: 30px"
+                >
+                    <img
+                        src="images/user-12.png"
+                        alt="image"
+                        class="float-right p-1 bg-white rounded-circle w-100"
+                    />
                 </figure>
-                <h4 class="fw-700 font-sm mt-2 mb-lg-5 mb-4 pl-15">Mohannad Zitoun <span
-                        class="fw-500 font-xssss text-grey-500 mt-1 mb-3 d-block">support@gmail.com</span>
+                <h4 class="fw-700 font-sm mt-2 mb-lg-5 mb-4 pl-15">
+                    Mohannad Zitoun
+                    <span
+                        class="fw-500 font-xssss text-grey-500 mt-1 mb-3 d-block"
+                        >support@gmail.com</span
+                    >
                 </h4>
-                <div class="d-flex align-items-center justify-content-center position-absolute-md right-15 top-0 me-2">
-                    <a href="#"
-                        class="d-none d-lg-block bg-success p-3 z-index-1 rounded-3 text-white font-xsssss text-uppercase fw-700 ls-3">Add
-                        Friend</a>
-                    <a href="#" class="d-none d-lg-block bg-greylight btn-round-lg ms-2 rounded-3 text-grey-700"><i
-                            class="feather-mail font-md"></i></a>
+                <div
+                    class="d-flex align-items-center justify-content-center position-absolute-md right-15 top-0 me-2"
+                >
+                    <a
+                        href="#"
+                        class="d-none d-lg-block bg-success p-3 z-index-1 rounded-3 text-white font-xsssss text-uppercase fw-700 ls-3"
+                        >Add Friend</a
+                    >
+                    <a
+                        href="#"
+                        class="d-none d-lg-block bg-greylight btn-round-lg ms-2 rounded-3 text-grey-700"
+                        ><i class="feather-mail font-md"></i
+                    ></a>
                 </div>
             </div>
-            <div class="card-body d-block w-100 shadow-none mb-0 p-0 border-top-xs">
-                <ul class="nav nav-tabs h55 d-flex product-info-tab border-bottom-0 ps-4" id="pills-tab" role="tablist">
-                    <li class="active list-inline-item me-5"><a
+            <div
+                class="card-body d-block w-100 shadow-none mb-0 p-0 border-top-xs"
+            >
+                <ul
+                    class="nav nav-tabs h55 d-flex product-info-tab border-bottom-0 ps-4"
+                    id="pills-tab"
+                    role="tablist"
+                >
+                    <li class="active list-inline-item me-5">
+                        <a
                             class="fw-700 font-xssss text-grey-500 pt-3 pb-3 ls-1 d-inline-block active"
-                            href="#navtabs1" data-toggle="tab">About</a></li>
+                            href="#navtabs1"
+                            data-toggle="tab"
+                            >About</a
+                        >
+                    </li>
                     <!-- <li class="list-inline-item me-5"><a class="fw-700 font-xssss text-grey-500 pt-3 pb-3 ls-1 d-inline-block"
                     href="#navtabs2" data-toggle="tab">Membership</a></li>
             <li class="list-inline-item me-5"><a class="fw-700 font-xssss text-grey-500 pt-3 pb-3 ls-1 d-inline-block"
@@ -42,9 +72,5 @@
     </div>
 </template>
 <script>
-export default {
-
-
-
-}
+export default {};
 </script>
