ec25d66ed6ed0fc4df726c38a5fa56d68bd0781b
abdo5200 <abdelrahman.mamdouh2200@gmail.com>
2024-12-09 02:04:15 +0200

    modified the add button to only appear on other people profile not the user's
    handled the image error in profile and cover pictures
    and the posts itself

--- resources/js/component/UserProfile.vue
+++ resources/js/component/UserProfile.vue
@@ -4,11 +4,10 @@
         <!-- <AboutUser /> -->
         <!-- <UserPhotos /> -->
         <!-- <div class="col-xl-8 col-xxl-9 col-lg-8"> -->
-            <CreatePost />
-            <PostContent />
+        <CreatePost />
+        <PostContent />
         <!-- </div> -->
     </div>
-
 </template>
 <script>
 import ProfileInformation from "./ProfileInformation.vue";
@@ -19,31 +18,31 @@ import PostContent from "./PostContent.vue";
 import axios from "axios";
 export default {
     components: {
-        "ProfileInformation": ProfileInformation,
-        "AboutUser": AboutUser,
-        "UserPhotos": UserPhotos,
-        "CreatePost": CreatePost,
-        "PostContent": PostContent,
-
+        ProfileInformation: ProfileInformation,
+        AboutUser: AboutUser,
+        UserPhotos: UserPhotos,
+        CreatePost: CreatePost,
+        PostContent: PostContent,
     },
-    data() {
-
-    },
-    methods: {
-
-    }, mounted() {
+    data() {},
+    methods: {},
+    mounted() {
         const path = window.location.pathname;
-        const segments = path.split('/');
+        const segments = path.split("/");
         const userId = segments[2];
         console.log(userId);
-        // current user_data 
-        axios.post(`http://127.0.0.1:8000/api/current/user/posts`, { "user_id": userId }).then((response) => {
-            console.log(response.data.posts);
-            this.$root.profileUser = response.data.profile_user_data;
-            console.log(this.$root.profileUser);
-            this.$root.current_posts = response.data.posts;
-        }).catch((error) => { });
-    }
-
-}
+        // current user_data
+        axios
+            .post(`http://127.0.0.1:8000/api/current/user/posts`, {
+                user_id: userId,
+            })
+            .then((response) => {
+                console.log(response.data.posts);
+                this.$root.profileUser = response.data.profile_user_data;
+                console.log(this.$root.profileUser);
+                this.$root.current_posts = response.data.posts;
+            })
+            .catch((error) => {});
+    },
+};
 </script>
